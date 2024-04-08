15e9f387ebdf3c59e52fb42cd2c8afef338167bd
bakiizese <bereketzeselassie@gmail.com>
2024-04-08 23:20:37 +0000

    fx

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const nt = true;
-    const nt2 = false;
-
-    task = nt;
-    task2 = nt2;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-     getNumberOfDepartments(list) {
+    getNumberOfDepartments(list) {
        return Object.keys(list).length;
      },
   };
