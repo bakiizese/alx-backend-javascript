3a4b9b6e502660f0064f45d6a8f04c5e4ad1a717
bakiizese <bereketzeselassie@gmail.com>
2024-04-08 22:08:06 +0000

    fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arra = [];
   for (const idx of array) {
-    const value = array[idx];
-    arra.push(appendString + value);
+    const value = array.indexOf(idx);
+    arra[value] = appendString + idx;
   }
+
   return arra;
 }
