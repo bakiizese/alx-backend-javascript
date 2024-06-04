6d0b9c3185a87dbde9d39d533c819c5dbd49b285
bakiizese <bereketzeselassie@gmail.come>
2024-06-04 04:57:56 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
-const readline = require('readline');
-
-const reads = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const ch = process.stdin.read();
+  if (ch) {
+    process.stdout.write(`Your name is: ${ch}`);
+  }
 });
-console.log('Welcome to Holberton School, what is your name?');
-reads.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-});
-reads.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
