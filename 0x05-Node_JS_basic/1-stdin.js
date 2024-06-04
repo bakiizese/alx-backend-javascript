dc5dfa696c395ad130162e7437606631d2230e63
bakiizese <bereketzeselassie@gmail.come>
2024-06-04 03:44:55 +0000

    try

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const reads = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+console.log('Welcome to Holberton School, what is your name?');
+reads.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  reads.close();
+});
+reads.on('close', () => {
+  console.log('This important software is now closing');
+});
