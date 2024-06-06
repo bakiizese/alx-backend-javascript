21f592c9184096fc078e075106a3fe2baecca0a7
bakiizese <bereketzeselassie@gmail.come>
2024-06-06 19:16:35 +0000

    f

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -18,36 +18,5 @@ describe ('Test for utils and sendPaymentRequestToApi integration', function () 
 		expect(result1).to.equal(result2);
 		stub.restore();
 		consolespy.log.restore();
-	})
-	it ('both should return the same result by the same args round a', function() {
-                const utils = sinon.spy(Utils.calculateNumber);
-                const payment = sinon.spy(sendPaymentRequestToApi);
-                const result1 = utils('SUM', 10.6, 10);
-                const result2 = payment(10.6, 10);
-		expect(payment.calledOnce).to.be.true;
-		expect(utils.calledOnce).to.be.true;
-		expect(result1).to.equal(21);
-                expect(result1).to.equal(result2);
-        })
-        it ('both should return the same result by the same args round b', function() {
-                const utils = sinon.spy(Utils.calculateNumber);
-                const payment = sinon.spy(sendPaymentRequestToApi);
-                const result1 = utils('SUM', 10, 10.4);
-                const result2 = payment(10, 10.4);
-                expect(payment.calledOnce).to.be.true;
-                expect(utils.calledOnce).to.be.true;
-		expect(result1).to.equal(20)
-                expect(result1).to.equal(result2);
-        })
-        it ('both should return the same result by the same args both round', function() {
-                const utils = sinon.spy(Utils.calculateNumber);
-                const payment = sinon.spy(sendPaymentRequestToApi);
-                const result1 = utils('SUM', 10.6, 10.4);
-                const result2 = payment(10.6, 10.4);
-                expect(payment.calledOnce).to.be.true;
-                expect(utils.calledOnce).to.be.true;
-                expect(result1).to.equal(21)
-                expect(result1).to.equal(result2);
-        });
-
+	});
 });
